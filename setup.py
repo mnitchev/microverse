a590d638cf657d8b8991b83f884ca85fc9e6526d
Iliya Zhechev <i4ko95@gmail.com>
2018-01-07 14:50:10 +0200

    Fix setup.py format

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 
 setup(
-  name="microverse",
-  version="0.1",
-  packages=find_packages(),
-  install_requires=[],
-  author="Mario Nitchev & Iliya Zhechev",
-  author_email="mail@ala.bala",
-  description="Genetic algorithms training neural networks",
-  license="MIT",
-  keywords="simulation, genetic, algorithm, neural network",
-  url="https://github.com/mnitchev/microverse",
-  test_suite="tests"
+    name='microverse',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['numpy>=1.11.1'],
+    author='Mario Nitchev & Iliya Zhechev',
+    author_email='mail@ala.bala',
+    description='Genetic algorithms training neural networks',
+    license='MIT',
+    keywords='simulation, genetic, algorithm, neural network',
+    url='https://github.com/mnitchev/microverse',
+    test_suite='tests'
 )
