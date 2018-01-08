0da70ef0e25166ff36be85ca7de3f932c335f8d2
Iliya Zhechev <i4ko95@gmail.com>
2018-01-08 22:07:46 +0200

    Implement a little more complex example with food and creature that can move, see, think and digest.

--- microverse/main.py
+++ microverse/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from simulator import *
+import simulator as sim
 import sys
 import math
 
@@ -15,24 +15,31 @@ def print(*args):
 
 
 def main():
-    creature = Creature(velocity=vec2(1, 0))
-    engine = Engine()
-    engine.add(creature)
+    environment = [
+        sim.Food(position=sim.vec2(100), size=100),
+        sim.Food(position=sim.vec2(20, 20), size=10)
+    ]
 
-    environment = [Creature(position=vec2(100), size=100)]
-
-    sight = Sight(
+    sight = sim.Sight(
         fov=math.pi / 2,
         ray_count=7,
         strength=250,
         environment=environment
     )
-    nn = NeuralNetwork([7, 10, 2])
-    navigator = Navigator(steering_magnitude=0.01)
+    brain = sim.NeuralNetwork([7, 10, 2])
+    navigator = sim.Navigator(steering_magnitude=0.01)
+    digestion = sim.Digestion(environment=environment)
+    mobility = sim.Mobility()
 
-    creature.plug(sight, nn, navigator)
+    creature = sim.Creature(velocity=sim.vec2(1, 0))
+    creature.plug(mobility)
+    creature.plug(sight, brain, navigator)
+    creature.plug(digestion)
 
-    @set_interval(1 / 30, start=True)
+    engine = sim.Engine()
+    engine.add(creature)
+
+    @sim.set_interval(1 / 30, start=True)
     def loop():
         print(creature.position.x, creature.position.y)
         engine.update()
