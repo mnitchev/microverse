e3ec0e1110e77f72ac84440dd66cac4e3c336c62
Mario <marionitchev@gmail.com>
2018-01-27 22:35:26 +0200

    Fix modules' __init__.py files.

--- microverse/simulator/__init__.py
+++ microverse/simulator/__init__.py
@@ -1,6 +1,6 @@
 from .agent import *
 from .plugins import *
-from .utils import *
+from .utils import geometry, color, vec2, set_interval
 from .agent import Agent
 from .engine import Engine
 from .renderer import Renderer
--- microverse/simulator/utils/__init__.py
+++ microverse/simulator/utils/__init__.py
@@ -1,4 +1,4 @@
 from .color import color
-from .geometry import *
+from .geometry import line_circle_intersect, line_line_intersect, ray_circle_intersect, circles_intersect
 from .set_interval import set_interval
 from .vec2 import vec2
